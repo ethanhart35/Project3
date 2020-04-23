c74f926d3fec47598c47652c57213e25cafae877
Max Lewis <max.a.lewis98@gmail.com>
2020-04-23 15:13:32 -0700

    static line graph installed using react-chartjs-2

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Books from "./pages/Books";
 import Nav from "./components/Nav";
+import Graph from "./components/Graph";
 
 function App() {
   return (
     <div>
       <Nav />
       <Books />
+      <Graph />
     </div>
   );
 }
