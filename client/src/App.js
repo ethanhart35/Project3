fbec3d32efece0980a82f84a0821807604853052
Zack Emerson <zacme429@gmail.com>
2020-04-25 12:53:07 -0600

    added max graph in homepage js fixed the stock nav so its under jumbotron on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "./components/Nav";
-import Graph from "./components/Graph";
+
 import HomePage from "./pages/HomePage/HomePage";
 import Articles from './pages/Articles';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -9,10 +9,11 @@ function App() {
   return (
     <div>
     <Nav /> //always going to show a nav component
-    <Graph />
+   
     <Router>
       <div>  
         <Route exact path='/' component={HomePage} />
+        
         <Route exact path="/articles" component={Articles} />
       </div>
     </Router>
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Jumbo from '../../components/Jumbotron'
 import StockNav from '../../components/StockNav';
+import Graph from "../../components/Graph";
+
 function HomePage(props) {
     return <>
     <Jumbo/>
-    <div id='charts' style={{height:"100vh"}}>
+    
     <StockNav/>
-    <img src = "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i7RJ3Q.PUDCM/v2/1000x-1.png" style={{height:"90vh"}}/>
-    </div>
+    <Graph/>
+    
     </>
   }
   
