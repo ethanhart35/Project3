b352b71a04aa418d1932f4c3f0de4588824919dd
Ethan Hart <ethanhart35@gmail.com>
2020-04-25 10:57:47 -0700

    fixed app.js conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,22 @@
 import React from "react";
-import Books from "./pages/Books";
 import Nav from "./components/Nav";
 import Graph from "./components/Graph";
-
+import HomePage from "./pages/HomePage/HomePage";
+import Articles from './pages/Articles';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      {/* <Nav />
-      <Books /> */}
-      <Graph />
-    </div>
-=======
-import HomePage from "./pages/HomePage/HomePage";
-import Articles from './pages/Articles';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Nav from './components/Nav';
-
-function App() {
-  return (
-
+    <Nav /> //always going to show a nav component
+    <Graph />
     <Router>
-      <>
-        <Nav /> //always going to show a nav component
+      <div>  
         <Route exact path='/' component={HomePage} />
         <Route exact path="/articles" component={Articles} />
-      </>
+      </div>
     </Router>
-
+    </div>
   );
 
 }
