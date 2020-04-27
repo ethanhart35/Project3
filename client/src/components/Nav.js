3a7f06a343232c4cf5990d2adca977f14ce7e164
desertdancer <steviepop@icloud.com>
2020-04-27 14:28:51 -0700

    style navbar and jumbotron

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,21 +2,21 @@ import React from "react";
 import "./Nav.css";
 
 const Nav = (props) => (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">FauxFinance</a>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" id="brand" href="/">FauxFinance</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="/articles">Articles</a>
+                    <a class="btn btn-primary py-0" href="/articles">Articles</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Forum</a>
+                    <a class="btn btn-primary py-0" href="#">Forum</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Sign In</a>
+                    <a class="btn btn-primary py-0" href="#">Sign In</a>
                 </li>
             </ul>
         </div>
