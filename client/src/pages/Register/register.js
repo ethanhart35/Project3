db4d323069b3655e65d0be4caf765b10ded7dc1f
Max Lewis <max.a.lewis98@gmail.com>
2020-04-27 12:29:14 -0700

    lots of reformating so the navbar and the routing now works as intended, added placeholder login and register peices

---
+++ client/src/pages/Register/register.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Register here</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Register
