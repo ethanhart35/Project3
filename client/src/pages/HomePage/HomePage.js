1918e9f8562e38e1096194cd64cc05aed1461037
desertdancer <steviepop@icloud.com>
2020-04-27 14:28:51 -0700

    style navbar and jumbotron

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -7,7 +7,7 @@ function HomePage(props) {
     <Jumbo/>
     <Graph />
     <div id='charts' style={{height:"100vh"}}>
-    <StockNav/>
+    {/* <StockNav/> */}
     <img src = "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i7RJ3Q.PUDCM/v2/1000x-1.png" style={{height:"90vh"}}/>
     </div>
     </>
