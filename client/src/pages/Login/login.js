416694f527345c96fbe090fca78aba6660f2249e
Max Lewis <max.a.lewis98@gmail.com>
2020-04-27 17:14:05 -0700

    aesthetic edits

--- client/src/pages/Login/login.js
+++ client/src/pages/Login/login.js
@@ -8,27 +8,21 @@ class Login extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col">
+                    <div className="col-md-6 offset-md-3">
                         <div className="card">
                             <div className="card-body">
-                                <h5 className="card-title">Register here</h5>
+                                <h5 className="card-title">Login</h5>
                                 <form>
                                     <div className="form-group">
                                         <label for="exampleInputEmail1">Email address</label>
                                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                                     </div>
                                     <div className="form-group">
                                         <label for="exampleInputPassword1">Password</label>
                                         <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
                                     </div>
-                                    <div className="form-check">
-                                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                                    </div>
-                                    <button type="submit" className="btn btn-primary">Submit</button>
+                                    <button type="submit" className="btn btn-primary">Login</button>
                                 </form>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
