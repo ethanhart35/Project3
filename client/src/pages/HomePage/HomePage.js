49bd23c2a1860b242a44da9000d3ed987d0b2745
Zack Emerson <zacme429@gmail.com>
2020-05-02 12:25:24 -0600

    got rid of img on homepage

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -8,7 +8,7 @@ function HomePage(props) {
     <Graph />
     <div id='charts' style={{height:"100vh"}}>
     {/* <StockNav/> */}
-    <img src = "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i7RJ3Q.PUDCM/v2/1000x-1.png" style={{height:"90vh"}}/>
+    
     </div>
     </>
   }
