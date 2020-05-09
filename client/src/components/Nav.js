d5fb0fecce978d23749e4074e5bc8a2e64b8b2b6
Zack Emerson <zacme429@gmail.com>
2020-05-09 11:38:20 -0600

    need to pull

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -15,12 +15,6 @@ const Nav = (props) => (
                 </li>
                 
                 <li className="nav-item">
-<<<<<<< HEAD
-                    <a className="nav-link" href="/login">Sign In</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/register">Register</a>
-=======
                     <a className="nav-link btn btn-primary py-0" href="/login">Log In</a>
                 </li>
                 <li className="nav-item">
@@ -32,7 +26,6 @@ const Nav = (props) => (
                 </li>
                 <li className="nav-item">
                     <a className="nav-link btn btn-primary py-0" href="/profile">Profile</a>
->>>>>>> 7c9ca15148000ed19e72710410c020fb87a146af
                 </li>
             </ul>
     </nav>
