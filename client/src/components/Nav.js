4526f7382f42308e2703c2e3b8a12bd6cd199b90
Zack Emerson <zacme429@gmail.com>
2020-05-09 13:28:24 -0600

    styling nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,29 +3,29 @@ import "./Nav.css";
 import API from "../utils/API";
 
 const Nav = (props) => (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar" id ="navbar">
         <a className="navbar-brand" href="/">FauxFinance</a>
 
         <ul className="nav">
                 <li className="nav-item active">
-                    <a className="nav-link" href="/articles">Articles</a>
+                    <a className="nav-link" id="articles-btn" href="/articles">Articles</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/market">Market</a>
+                    <a className="nav-link" id="market-btn" href="/market">Market</a>
                 </li>
                 
                 <li className="nav-item">
-                    <a className="nav-link btn btn-primary py-0" href="/login">Log In</a>
+                    <a className="nav-link btn  py-0" id="login-btn" href="/login">Log In</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link btn btn-primary py-0" onClick={API.logoutUser}>Log out</a>
+                    <a className="nav-link btn  py-0" id="logout-btn" onClick={API.logoutUser}>Log out</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link btn btn-primary py-0" href="/profile">Profile</a>
+                    <a className="nav-link btn  py-0" id="profile-btn" href="/profile">Profile</a>
                 </li>
                           
                 <li className="nav-item">
-                    <a className="nav-link btn btn-danger py-0"  id="btn"  href="/register">Register</a>
+                    <a className="nav-link btn py-0"  id="register-btn"  href="/register">Register</a>
                 </li>
 
                 {/* <li className="nav-item"> */}
