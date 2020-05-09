92cbf2d9285b2c1cf59456ab51a43a2b55198023
desertdancer <steviepop@icloud.com>
2020-05-09 08:36:36 -0700

    image and color options

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -3,8 +3,8 @@ import './Jumbotron.css';
 
 export default ()=> <div className="jumbotron">
 <h1 className="display-4 font-italic">FauxFinance</h1>
-<p className="lead">Try our simulator</p>
+<p className="lead">Planning for the Future?</p>
 <hr className="my-4"/>
-<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+<p>Use these tools and information to make educated decisions.</p>
 <a className="btn btn-warning btn-lg" href="#charts" role="button">More</a>
 </div>
