51f2060de1fd3799f564ad04fde818a03addcdf0
Laura Rodela <laurarodela@Lauras-MacBook-Pro-2.local>
2020-05-09 12:13:02 -0700

    added html and css file for articles

--- client/src/pages/Articles/articles.js
+++ client/src/pages/Articles/articles.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from '../../components/Nav';
 import API from '../../utils/API';
+
 // var cheerio = require("cheerio");
 var axios = require("axios");
 var results=[];
