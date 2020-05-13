b1c6873f10b5959d26c1c87204240f1f26965101
Zack Emerson <zacme429@gmail.com>
2020-05-12 20:19:10 -0700

    styling the nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -20,15 +20,11 @@ const Nav = (props) => (
                     <a className="nav-link btn  py-0" id="logout-btn" onClick={API.logoutUser}>Log out</a>
                 </li>
                 <li className="nav-item">
-<<<<<<< HEAD
                     <a className="nav-link btn  py-0" id="profile-btn" href="/profile">Profile</a>
                 </li>
                           
                 <li className="nav-item">
                     <a className="nav-link btn py-0"  id="register-btn"  href="/register">Register</a>
-=======
-                    <a className="nav-link btn btn-danger py-0"  id="btn"  href="/register">Register</a>
->>>>>>> 3ff7a424e6cec79e990f8e34d0b8c33059227e2e
                 </li>
             </ul>
     </nav>
